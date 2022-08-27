135fbf33d6b0a92cb615fbaf421be9a34080cea9
NotEternal <cent1pede@protonmail.ch>
2022-08-27 01:55:35 -0700

    Update code-trash

--- src/code-trash/ts/list.ts
+++ src/code-trash/ts/list.ts
@@ -1,6 +1,19 @@
-interface ListNode {
-  val: any;
-  next: ListNode | null;
+/**
+ * singly-linked list:
+ *
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+interface ListNode<ValT = any> {
+  val: ValT;
+  next: ListNode<ValT> | null;
 }
 
 enum Condition {
@@ -40,4 +53,34 @@ function middleNode(head: ListNode["next"]): ListNode["next"] {
   const [result] = listLoopUntil(head, Condition.untilNumber, middle);
 
   return result;
+}
+
+type CommonNode = ListNode | null;
+
+function mergeTwoLists(list1: CommonNode, list2: CommonNode): CommonNode {
+  let [currentNode, checkedNode] =
+    list1?.val < list2?.val ? [list1, list2] : [list2, list1];
+  const headOfMerged = currentNode;
+
+  while (true) {
+    if (!checkedNode) break;
+
+    if (!currentNode?.next) {
+      if (checkedNode?.next) currentNode!.next = checkedNode.next;
+      break;
+    }
+
+    if (currentNode.next.val >= checkedNode.val) {
+      const tmpNode1 = currentNode.next;
+      const tmpNode2 = checkedNode.next;
+
+      currentNode.next = checkedNode;
+      checkedNode.next = tmpNode1;
+      checkedNode = tmpNode2;
+    } else {
+      currentNode = currentNode.next;
+    }
+  }
+
+  return headOfMerged;
 }
