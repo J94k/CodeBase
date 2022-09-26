8be3a735c05887bbd6f803421dcfe6b3eb567b46
NotEternal <cent1pede@protonmail.ch>
2022-09-26 08:55:16 +0200

    feat(code-trash): add new solutions

--- src/code-trash/ts/numbers.ts
+++ src/code-trash/ts/numbers.ts
@@ -43,3 +43,30 @@ function subtractProductAndSum(n: number): number {
 function hammingWeight(n: number): number {
   return n.toString(2).match(/1/g)?.length ?? 0;
 }
+
+type Coordinate = number[];
+
+const getManhattanDistance = (coord1: Coordinate, coord2: Coordinate) => {
+  return Math.abs(coord1[0] - coord2[0]) + Math.abs(coord1[1] - coord2[1]);
+};
+
+function nearestValidPoint(x: number, y: number, points: Coordinate[]): number {
+  let index;
+  let minDistance;
+  const pointsWithoutInvalid = points.map((p) => (p[0] === x || p[1] === y ? p : null));
+
+  for (let i = 0; i < pointsWithoutInvalid.length; i++) {
+    const p = pointsWithoutInvalid[i];
+
+    if (!p) continue;
+
+    const d = getManhattanDistance([x, y], p);
+
+    if (typeof minDistance !== 'number' || d < minDistance) {
+      minDistance = d;
+      index = i;
+    }
+  }
+
+  return typeof index === 'number' ? index : -1;
+}
